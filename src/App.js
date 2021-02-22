524647460883079393c86a0c1592186fa13753e6
erickacostab0104 <erickacostab0104@gmail.com>
2021-02-21 20:15:53 -0500

    archivos subiendo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Header } from "./componentes/Header";
+import { ProductosLista } from "./componentes/Productos/index";
+import 'boxicons';
+import { BrowserRouter as Router } from "react-router-dom";
+import {Paginas} from "./componentes/Paginas";
+import { Dataprovider } from "./context/Dataprovider";
+import { Carrito } from "./componentes/Carrito";
 
 function App() {
   return (
+    <Dataprovider>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <Header />
+      <Carrito />
+      <Paginas />
+      </Router>
     </div>
+    </Dataprovider>
   );
 }
 
